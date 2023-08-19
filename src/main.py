e80a7ec5cbda36edc5af35d8d186bf53f08d27b0
Yokoo-arch <ambroise.laux@gmail.com>
2023-08-19 23:17:52 +0200

    Added license file + changed readme.md

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 """
-Wrote by Yokoo-arch 2023 (https://github.com/Yokoo-arch)
+Wrote by Yokoo-arch 2023 (https://github.com/Yokoo-arch).
+Github repository: https://github.com/Yokoo-arch/PyMacChanger.
+If you have any issues, please feel free to open an issue on the Github repository.
 """
 
 import subprocess
